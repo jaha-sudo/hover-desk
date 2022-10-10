dcdc7f80f8ab8797a05c3e5a0200752e5d300ebd
jaha-sudo <romanovajaha@gmail.com>
2022-10-10 11:32:42 +0500

    correct code

--- app.js
+++ app.js
@@ -18,25 +18,27 @@ const colors = [
 for (let i = 0; i < squares_number; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
-  square.addEventListener("mouseover", () => setColor(square));
 
-  square.addEventListener("mouseleave", () => removeColor(square));
+  square.addEventListener("mouseover", setColor);
+
+  square.addEventListener("mouseleave", removeColor);
 
   board.append(square);
 }
 
-function setColor(element) {
+function setColor(event) {
+  const element = event.target;
   const color = getRandomColor();
   element.style.background = color;
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function removeColor(element) {
+function removeColor(event) {
+  const element = event.target;
   element.style.background = "#1d1d1d";
   element.style.boxShadow = `0 0 2px #000`;
 }
 
 function getRandomColor() {
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
